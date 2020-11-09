a3cb70f3853d4e38b36f8dc07e5a2819d10a4726
Luan Medeiros Silveira <luanmedeirossilveira@gmail.com>
2020-11-09 00:46:49 -0300

    dsasad

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import Navbar from '../Components/Main/Navbar';
 import Footer from '../Components/Main/Footer'
 import '../styles/globals.css'
 import '../styles/navbar.css'
+import '../styles/dashboard.css'
 
 function MyApp({ Component, pageProps }) {
   return (
